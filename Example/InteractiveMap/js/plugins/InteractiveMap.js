1966503f0cd76e6a3e62d7f7f2428cc6b2fb9c78
Hime <hime@blueaxolotl.com>
2016-09-28 21:23:36 +0200

    Initial commit

---
+++ Example/InteractiveMap/js/plugins/InteractiveMap.js
@@ -0,0 +1,192 @@
+/*jslint vars: true, nomen: true, plusplus: true, eqeq: true */
+/*globals console*/
+/*globals Scene_Map*/
+/*globals Graphics, Sprite, Bitmap, TouchInput, Game_Interpreter, PluginManager, ImageManager, SceneManager */
+/*globals $gameMap, $gameSwitches */
+//=============================================================================
+// Menu.js
+//=============================================================================
+
+/*:
+ * @plugindesc Pivert Interactive map.
+ * @author Pivert
+ *
+ * @param Images directory
+ * @desc The directory where map images are stored
+ * @default img/map
+ *
+ * @param Font size
+ * @desc The font size of map texts
+ * @default 16
+ *
+ */
+
+(function () {
+
+    "use strict";
+
+    var parameters = PluginManager.parameters('InteractiveMap');
+
+    var imageDirectory = String(parameters['Images directory'] || 'img/map');
+    var fontSize = Number(parameters['Font size'] || 16);
+
+    var map;
+
+    //=============================================================================
+    // Interactive Map
+    //=============================================================================
+    var Scene_Map_update = Scene_Map.prototype.update;
+    Scene_Map.prototype.update = function () {
+        Scene_Map_update.call(this);
+
+        if (this._showMap) {
+
+            var i, x, y, w, h;
+
+            var tX = TouchInput._cursorX;
+            var tY = TouchInput._cursorY;
+
+            document.body.style.cursor = 'inherit';
+
+            for (i = 0; i < map.length; i++) {
+                x = map[i].x;
+                y = map[i].y;
+                w = map[i].image.width;
+                h = map[i].image.height;
+
+                if (!map[i].trigger || $gameSwitches.value(map[i].trigger)) {
+                    this._sprites[map[i].id].visible = true;
+                }
+
+                if (tX >= x && tX <= x + w && tY >= y && tY <= y + h) {
+
+                    if (!map[i].trigger || $gameSwitches.value(map[i].trigger)) {
+                        document.body.style.cursor = 'pointer';
+                        this._texts[map[i].id].visible = true;
+                    }
+
+                    if (TouchInput.isTriggered() && (!map[i].trigger || $gameSwitches.value(map[i].trigger))) {
+                        var event = $gameMap.event(map[i].event);
+                        if (event) {
+                            event.start();
+                            break;
+                        }
+                    }
+                } else {
+                    this._texts[map[i].id].visible = false;
+                }
+            }
+        }
+    };
+
+    Scene_Map.prototype.initSprites = function () {
+        var i, x, y, w, h;
+
+        this._sprites = {};
+        this._texts = {};
+
+        // Image
+        for (i = 0; i < map.length; i++) {
+            x = map[i].x;
+            y = map[i].y;
+            w = map[i].image.width;
+            h = map[i].image.height;
+
+            var imageSprite = new Sprite(map[i].image);
+            imageSprite.x = x;
+            imageSprite.y = y;
+            imageSprite.visible = !map[i].trigger || $gameSwitches.value(map[i].trigger);
+            this.addChild(imageSprite);
+            this._sprites[map[i].id] = imageSprite;
+        }
+
+        // Text
+        for (i = 0; i < map.length; i++) {
+            x = map[i].x;
+            y = map[i].y;
+            w = map[i].image.width;
+            h = map[i].image.height;
+
+            var offset = Graphics.boxWidth - (x + w) - 5;
+            x -= offset;
+            w += 2 * offset;
+            if (map[i].textPosition === 'top') {
+                y -= 40;
+            } else if (map[i].textPosition === 'bottom') {
+                y += h;
+            }
+
+            var bitmap = new Bitmap(w, 50);
+            bitmap.fontSize = fontSize;
+            bitmap.drawText(map[i].name, 0, 6, w, 20, 'center');
+
+            var textSprite = new Sprite(bitmap);
+            textSprite.x = x;
+            textSprite.y = y;
+            textSprite.visible = false;
+            this.addChild(textSprite);
+            this._texts[map[i].id] = textSprite;
+        }
+    };
+
+    Scene_Map.prototype.showMap = function () {
+        this._showMap = true;
+        if (!this._init) {
+            this._init = true;
+            this.initSprites();
+        }
+    };
+
+    //=============================================================================
+    // Touch input
+    //=============================================================================
+    var TouchInput_onMouseMove = TouchInput._onMouseMove;
+    TouchInput._onMouseMove = function (event) {
+        TouchInput_onMouseMove.call(this, event);
+        
+        this._cursorX = Graphics.pageToCanvasX(event.pageX);
+        this._cursorY = Graphics.pageToCanvasY(event.pageY);
+    };
+
+    var loadMaps = function () {
+
+        var xhr = new XMLHttpRequest();
+        xhr.overrideMimeType('application/json');
+        xhr.open('GET', 'data/InteractiveMap.json');
+
+        xhr.onload = function () {
+            if (xhr.status < 400) {
+                try {
+                    map = JSON.parse(xhr.responseText);
+                    var i;
+                    for (i = 0; i < map.length; i++) {
+                        map[i].image = ImageManager.loadBitmap(imageDirectory + '/', map[i].image);
+                    }
+                } catch (e) {
+                    console.error('Could not load script');
+                    console.error(e);
+                }
+            } else {
+                console.error('File data/InteractiveMap.json not found');
+            }
+        };
+
+        xhr.onerror = function (e) {
+            console.error('Error while fetching data/InteractiveMap.json');
+            console.error(e);
+        };
+
+        xhr.send();
+    };
+
+    loadMaps();
+
+    var _Game_Interpreter_pluginCommand = Game_Interpreter.prototype.pluginCommand;
+    Game_Interpreter.prototype.pluginCommand = function (command, args) {
+        _Game_Interpreter_pluginCommand.call(this, command, args);
+        if (command === 'InteractiveMap' && args[0] === 'open' && map) {
+            SceneManager._scene.showMap();
+        }
+    };
+
+}());
--- README.md
+++ README.md
@@ -1,2 +1,78 @@
-# RPG-Maker-MV-InteractiveMap
-Interactive map system form RPG Maker MV
+# RPG-Maker-MV-Achievements
+
+RPG Maker MV Achievements allows you to create achievements in RPG Maker MV.
+
+# Usage
+
+To open the interactive, use the module command "InteractiveMap open".
+
+![InteractiveMap](https://raw.githubusercontent.com/PicusViridis/RPG-Maker-MV-InteractiveMap/master/Screenshots/Interactive%20Map.png)
+
+Map images must be png files
+
+# InteractiveMap.json
+
+Place a file named InteractiveMap.json under the data directory. InteractiveMap.json must use UTF-8 encoding (you can change encoding when you save it with notepad.exe).
+
+This file must be structured like that :
+
+```json
+[
+  {
+    "id": 1,
+    "name": "Airport",
+    "image": "airport",
+    "x": 382,
+    "y": 430,
+	"textPosition": "bottom",
+	"event": 1
+  }
+]
+```
+
+_If you need documentation about json files structure, please refer to : http://www.w3schools.com/json/_
+
+_If you need to validate your json file (i.e. check that its structure is correct), you can use : https://jsonformatter.curiousconcept.com/_
+
+__id__
+
+Id of the map item.
+
+__name__
+
+Name of the map item.
+
+__image__
+
+Image of the map item.
+
+__x__
+
+X position of the map item.
+
+__y__
+
+Y position of the map item.
+
+__textPosition__
+
+Position of the name (top or bottom).
+
+__event__
+
+ID of the event launched when clicking on the map item.
+
+# Module settings
+
+__Images directory__
+
+Interactive map images directory. Default is img/map.
+
+__Map image__
+
+Background of the interactive map. Default is "map".
+
+
+# Credits
+
+Feel free to use this script is commercial or non commercial games, but please give credits to Pivert somewhere in your game =)
